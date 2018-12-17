eeb1a38f12da11dbd16c0d8d7b6a8e93913773e2
pg07codes <pg07codes@gmail.com>
2018-12-17 16:43:16 +0530

    done

--- app/src/main/java/com/example/pg07codes/mizz/MainActivity.java
+++ app/src/main/java/com/example/pg07codes/mizz/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,15 +35,35 @@ public class MainActivity extends AppCompatActivity {
         tweetAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,tweetArray);
 
         tweets.setAdapter(tweetAdapter);
+
+        tweets.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView<?> arg0, View v, int index, long arg3) {
+                tweetArray.remove(tweets.getItemAtPosition(index).toString());
+                Toast.makeText(MainActivity.this,"ITEM DELETED", Toast.LENGTH_SHORT).show();
+                tweetAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+        tweets.setLongClickable(true);
     }
 
     public void addTweet(View v){
         String x=tweet.getText().toString();
-        add(x);
+        if(x.equals("")){
+            Toast.makeText(this, "empty tweet",Toast.LENGTH_SHORT).show();
+        }else{
+            add(x);
+            tweet.setText("");
+        }
+
     }
 
     public void add(String t){
         tweetArray.add(t);
         tweetAdapter.notifyDataSetChanged();
     }
+
+
 }
