927ebbd1dc7017e8c8de4ddfb6d085dcff62c78f
pg07codes <pg07codes@gmail.com>
2018-12-17 16:51:52 +0530

    readme added

--- app/src/main/java/com/example/pg07codes/mizz/MainActivity.java
+++ app/src/main/java/com/example/pg07codes/mizz/MainActivity.java
@@ -2,68 +2,13 @@ package com.example.pg07codes.mizz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText tweet;
-    Button post;
-    ListView tweets;
-    ArrayList<String> tweetArray;
-    ArrayAdapter<String> tweetAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // declaring vars here
-        tweet=findViewById(R.id.tweet);
-        post=findViewById(R.id.post);
-        tweets=findViewById(R.id.tweets);
-        tweetArray = new ArrayList<String>();
-        tweetAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,tweetArray);
-
-        tweets.setAdapter(tweetAdapter);
-
-        tweets.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-
-            public boolean onItemLongClick(AdapterView<?> arg0, View v, int index, long arg3) {
-                tweetArray.remove(tweets.getItemAtPosition(index).toString());
-                Toast.makeText(MainActivity.this,"ITEM DELETED", Toast.LENGTH_SHORT).show();
-                tweetAdapter.notifyDataSetChanged();
-                return true;
-            }
-        });
-
-        tweets.setLongClickable(true);
     }
-
-    public void addTweet(View v){
-        String x=tweet.getText().toString();
-        if(x.equals("")){
-            Toast.makeText(this, "empty tweet",Toast.LENGTH_SHORT).show();
-        }else{
-            add(x);
-            tweet.setText("");
-        }
-
-    }
-
-    public void add(String t){
-        tweetArray.add(t);
-        tweetAdapter.notifyDataSetChanged();
-    }
-
-
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# this repo contains multiple branches of some apps i built to practice android programming
+
+# - (on master)a simple task-list built to practice android programming.
+
+- add new tweet(task)
+- longPress it to delete task
+- ListView used to display tasks
